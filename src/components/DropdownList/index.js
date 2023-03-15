c1e0cb133c22e7d412b36772482d479261d2d53d
Icaro <luizicaro.s@gmail.com>
2023-03-15 18:16:25 -0300

    Adding required parameter

--- src/components/DropdownList/index.js
+++ src/components/DropdownList/index.js
@@ -4,7 +4,7 @@ function DropdownList(props) {
   return (
     <div className='dropdown-list'>
       <label>{props.label}</label>
-      <select>
+      <select required={props.required}>
         {props.items.map((item) => <option key={item}>{item}</option>)}
       </select>
     </div>
