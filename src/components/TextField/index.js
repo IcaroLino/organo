c1910f7c10f780bb68493deb29cea3432c44f969
Icaro <luizicaro.s@gmail.com>
2023-03-15 16:33:47 -0300

    Create TextField component

---
+++ src/components/TextField/index.js
@@ -0,0 +1,12 @@
+import './TextField.css'
+
+function TextField(props) {
+  return (
+    <div className="text-field">
+      <label>{props.label}</label>
+      <input placeholder={props.placeholder} />
+    </div>
+  );  
+}
+
+export default TextField;
