e26171e3dee57e78f05e54a3401eaff52750882c
Icaro <luizicaro.s@gmail.com>
2023-03-24 16:59:08 -0300

    Implement input to change background color of squad component

--- src/components/Squad/index.js
+++ src/components/Squad/index.js
@@ -1,12 +1,14 @@
+import hexToRgba from 'hex-to-rgba';
 import MemberCard from '../MemberCard';
 import './Squad.css'
 
 function Squad(props) {
   return (
-    (props.members.length > 0) && <section className='squad' style={{ backgroundColor: props.secondaryColor }}>
-      <h3 style={{ borderColor: props.primaryColor }}>{props.name}</h3>
+    (props.members.length > 0) && <section className='squad' style={{ backgroundColor: hexToRgba(props.squad.color, 0.6) }}>
+      <input type='color' className='input-color' value={props.squad.color} onChange={(event) => props.changeColor(event.target.value, props.squad.id)} />
+      <h3 style={{ borderColor: props.squad.color }}>{props.squad.name}</h3>
       <div className='members'>
-        {props.members.map((member) => <MemberCard key={member.name} headerColor={props.primaryColor} member={member} remove={props.remove} />)}
+        {props.members.map((member) => <MemberCard key={member.name} headerColor={props.squad.color} member={member} remove={props.remove} />)}
       </div>
     </section>
   );  
