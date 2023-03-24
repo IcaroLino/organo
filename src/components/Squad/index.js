d2efe623553f417ad6dc5e0a485e57f43604e5a0
Icaro <luizicaro.s@gmail.com>
2023-03-24 20:39:51 -0300

    Adding background image to Squad component

--- src/components/Squad/index.js
+++ src/components/Squad/index.js
@@ -4,7 +4,7 @@ import './Squad.css'
 
 function Squad(props) {
   return (
-    (props.members.length > 0) && <section className='squad' style={{ backgroundColor: hexToRgba(props.squad.color, 0.6) }}>
+    (props.members.length > 0) && <section className='squad' style={{ backgroundColor: hexToRgba(props.squad.color, 0.6), backgroundImage: "url(/images/bg.png)" }}>
       <input type='color' className='input-color' value={props.squad.color} onChange={(event) => props.changeColor(event.target.value, props.squad.id)} />
       <h3 style={{ borderColor: props.squad.color }}>{props.squad.name}</h3>
       <div className='members'>
